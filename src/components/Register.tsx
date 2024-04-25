0ee0a664c0e17ffd0aaefd4cd63a3d3f838cf576
kaiyi17 <135378874+kaiyi17@users.noreply.github.com>
2024-04-25 08:35:10 -0400

    Update Register.tsx

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,39 +1,38 @@
-import React, { useState, ChangeEvent, FormEvent }from 'react';
-import { OAuthButtonGroup } from './OAuthButtonGroup';
-import { RegisterPassword } from './RegisterPassword';
-import { Link } from 'react-router-dom';
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { PasswordField } from "./PasswordField";
+import { OAuthButtonGroup } from "./OAuthButtonGroup";
 
 export const Register = () => {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: ''
-  });
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleRegister = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     try {
-      const response = await fetch('http://localhost:8080/api/auth/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
-      if (!response.ok) {
-        throw new Error('Registration failed');
-      }
-      window.location.href = '/login';
+      const response = await axios.post(
+        "http://localhost:8080/api/auth/register",
+        {
+          email: email,
+          password: password,
+          username: username,
+        }
+      );
+      console.log("Registration successful", response.data);
+      alert("Registration successful");
+      navigate("/login");
     } catch (error: any) {
-      console.error('Error during registration:', error.message);
+      console.error(
+        "Registration error:",
+        error.response?.data || error.message
+      );
+
+      alert(
+        "Registration error: " + (error.response?.data.message || error.message)
+      );
     }
   };
 
@@ -43,41 +42,67 @@ export const Register = () => {
         <div className="col-md-6">
           <div className="text-center">
             <h2 className="mt-4 mb-3">Create an account</h2>
-            <p className="mb-4">Already have an account? <Link to="/login" className="text-decoration-none">Log in</Link></p>
+            <p className="mb-4">
+              Already have an account?{" "}
+              <Link to="/login" className="text-decoration-none">
+                Log in
+              </Link>
+            </p>
           </div>
           <div className="p-4 bg-light rounded shadow-sm">
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleRegister}>
               <div className="mb-4">
-                <label htmlFor="username" className="form-label">Username</label>
-                <input type="text" className="form-control" id="username" name="username" value={formData.username} onChange={handleChange} />
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </div>
               <div className="mb-4">
-                <label htmlFor="email" className="form-label">Email</label>
-                <input type="email" className="form-control" id="email" name="email" value={formData.email} onChange={handleChange} />
+                <label htmlFor="username" className="form-label">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                />
               </div>
               <div className="mb-4">
-                <label htmlFor="password" className="form-label">Password</label>
-                <RegisterPassword id="password" name="password" value={formData.password} onChange={handleChange} autoComplete="new-password" required />
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <PasswordField
+                  id="password"
+                  name="password"
+                  autoComplete="new-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
-              <div className="mb-3 form-check d-flex justify-content-between">
-                <div>
-                  <input type="checkbox" className="form-check-input" id="termsAgreement" required />
-                  <label className="form-check-label" htmlFor="termsAgreement">I agree to the <a href="#" className="text-decoration-none">terms and conditions</a></label>
-                </div>
-              </div>
-              <button type="submit" className="btn btn-primary btn-lg w-100 mb-4">Sign up</button>
-              <hr />
-              <p className="text-center mb-3">or continue with</p>
-              <div className="btn-group d-grid gap-2">
-                <OAuthButtonGroup />
-              </div>
+              <button
+                type="submit"
+                className="btn btn-primary btn-lg w-100 mb-4"
+              >
+                Sign up
+              </button>
             </form>
           </div>
         </div>
+        <OAuthButtonGroup />
       </div>
     </div>
   );
 };
 
 export default Register;
-
