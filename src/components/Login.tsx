27b74ba20c78e1f5b91175e923f298342926e17e
kaiyi17 <135378874+kaiyi17@users.noreply.github.com>
2024-04-25 08:34:05 -0400

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,95 +1,115 @@
+import React, { useState } from "react";
+import axios from "axios";
 import { OAuthButtonGroup } from "./OAuthButtonGroup";
 import { PasswordField } from "./PasswordField";
-import { Link } from "react-router-dom";
-import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useNavigate } from "react-router-dom";
 
-export const Login = () => {
+interface LoginProps {
+  onLogin: (user: UserType) => void;
+}
 
-  const [formData, setFormData] = useState({
-    password: '',
-    username: ''
-  });
+export const Login: React.FC<LoginProps> = ({ onLogin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
     try {
-      const response = await fetch('http://localhost:8080/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
-      if (!response.ok) {
-        throw new Error('Login failed');
-      }
-      window.location.href = '/home';
-      // Redirect to dashboard or any other page on successful login
+      const response = await axios.post(
+        "http://localhost:8080/api/auth/login",
+        {
+          email,
+          password,
+        }
+      );
+      console.log("Login successful", response.data);
+      localStorage.setItem("token", response.data.token);
+      localStorage.setItem("user", JSON.stringify(response.data.user));
+
+      const userData: UserType = response.data.user;
+      onLogin(userData);
+      navigate("/dashboard");
     } catch (error: any) {
-      console.error('Error during login:', error.message);
+      console.error("Login error", error.response?.data || error.message);
+      alert("Login failed: " + (error.response?.data.message || error.message));
     }
   };
 
-  return(
+  return (
     <div className="container-lg py-4">
-    <div className="row justify-content-center">
-      <div className="col-md-6">
-        <div className="text-center">
-          <h2 className="mt-4 mb-3">Log in to your account</h2>
-          <p className="mb-4">
-            Don't have an account?{" "}
-            <Link to="/register" className="text-decoration-none">
-              Sign up
-            </Link>
-          </p>
-        </div>
-        <div className="p-4 bg-light rounded shadow-sm">
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <label htmlFor="username" className="form-label">
-                Username
-              </label>
-              <input type="text" className="form-control" id="username" name="username" value={formData.username} onChange={handleChange} />
-            </div>
-            <div className="mb-4">
-              <label htmlFor="password" className="form-label">
-                Password
-              </label>
-              <input type="password" className="form-control" id="password" name="password" value={formData.password} onChange={handleChange} autoComplete="current-password" required />
-            </div>
-            <div className="mb-3 form-check d-flex justify-content-between">
-              <div>
-                <input type="checkbox" className="form-check-input" id="rememberMe" defaultChecked />
-                <label className="form-check-label" htmlFor="rememberMe">
-                  Remember me
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="text-center">
+            <h2 className="mt-4 mb-3">Log in to your account</h2>
+            <p className="mb-4">
+              Don't have an account?{" "}
+              <Link to="/register" className="text-decoration-none">
+                Sign up
+              </Link>
+            </p>
+          </div>
+          <div className="p-4 bg-light rounded shadow-sm">
+            <form onSubmit={handleLogin}>
+              <div className="mb-4">
+                <label htmlFor="email" className="form-label">
+                  Email
                 </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
               </div>
-              <a href="#" className="text-decoration-none">
-                Forgot password?
-              </a>
-            </div>
-            <button type="submit" className="btn btn-primary btn-lg w-100 mb-4">
-              Sign in
-            </button>
-            <hr />
-            <p className="text-center mb-3">or continue with</p>
-            <div className="btn-group d-grid gap-2">
-              <OAuthButtonGroup />
-            </div>
-          </form>
+              <div className="mb-4">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <PasswordField
+                  id="password"
+                  name="password"
+                  autoComplete="current-password"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="mb-3 form-check d-flex justify-content-between">
+                <div>
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="rememberMe"
+                    defaultChecked
+                  />
+                  <label className="form-check-label" htmlFor="rememberMe">
+                    Remember me
+                  </label>
+                </div>
+                <a href="#" className="text-decoration-none">
+                  Forgot password?
+                </a>
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary btn-lg w-100 mb-4"
+              >
+                Sign in
+              </button>
+              <hr />
+              <p className="text-center mb-3">or continue with</p>
+              <div className="btn-group d-grid gap-2">
+                <OAuthButtonGroup />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Login;
